f4d479723bba44f178066af47e63d40955dbc576
tringak <tringaakrasniqi@gmail.com>
2020-06-06 22:18:29 +0200

    Setup app tv_shows

--- main/main/settings.py
+++ main/main/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.tv_shows',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
